bddae8a18322cebf0ca71a646679e6370dc37219
Eyal Biran <eyalbira@gmail.com>
2016-10-23 11:22:51 +0300

    Add Toolbar to the example Activity (with menu button support)

--- slidesidemenu-samples/src/main/java/com/lemonade/widgets/slidesidemenu/samples/SlideSideMenuExampleActivity.java
+++ slidesidemenu-samples/src/main/java/com/lemonade/widgets/slidesidemenu/samples/SlideSideMenuExampleActivity.java
@@ -1,7 +1,9 @@
 package com.lemonade.widgets.slidesidemenu.samples;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.lemonade.widgets.slidesidemenu.SlideSideMenuTransitionLayout;
@@ -9,13 +11,29 @@ import com.lemonade.widgets.slidesidemenu.SlideSideMenuTransitionLayout;
 public class SlideSideMenuExampleActivity extends AppCompatActivity {
 
     private SlideSideMenuTransitionLayout mSlideSideMenu;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_slide_side_menu_example);
 
+        // Grab the widget
         mSlideSideMenu = (SlideSideMenuTransitionLayout)findViewById(R.id.slide_side_menu);
+
+        // Setup the toolbar
+        mToolbar = (Toolbar) findViewById(R.id.base_toolbar);
+        setSupportActionBar(mToolbar);
+        mToolbar.setTitleTextColor(Color.WHITE);
+
+        // Wire SideMenu with Toolbar
+        mToolbar.setNavigationIcon(R.drawable.ic_menu);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSlideSideMenu.toggle();
+            }
+        });
     }
 
     @Override
